cbe66b99a82d3aa7a9ac4d1c6a40ac2363543c71
Nainika-V <nainikavempalli@gmail.com>
2025-03-18 23:07:40 +0530

    navigation bar

---
+++ app/src/main/java/com/example/hostel/HostelsList.java
@@ -0,0 +1,37 @@
+package com.example.hostel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HostelsList extends AppCompatActivity {
+    public Button Seethamma_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hostels_list);
+        Seethamma_button = findViewById(R.id.Seethamma_button);
+        Seethamma_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HostelsList.this, Mainpage.class);
+                Toast.makeText(HostelsList.this, "success", Toast.LENGTH_SHORT).show();
+                startActivity(i);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.hostel), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
